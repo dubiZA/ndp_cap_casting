163e2581366da19b08e7e2f8d8f60803b189d77c
dubiZA <iamdzh@outlook.com>
2020-07-23 21:01:14 -0600

    Start setup of db models

--- models.py
+++ models.py
@@ -1,1 +1,28 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+# Initialize DB connection details
+database_name = 'casting_dev'
+database_path = f'postgresql:///{database_name}'
+
+
+db = SQLAlchemy()
+
+# Setup the database connection
+def setup_db(app, database_path=database_path):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+# Define the Models for the databases
+class Movie(db.Model):
+    __tablename__ = "movies"
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
