32001d06371eba41ccafa2c85427b78886d522cb
dubiZA <iamdzh@outlook.com>
2020-07-30 09:24:23 -0600

    Add global test data to setUp and tearDown

--- test_app.py
+++ test_app.py
@@ -6,12 +6,13 @@ from flask_sqlalchemy import SQLAlchemy
 from app import create_app
 from models import setup_db, Actor, Movie
 
+unittest.TestLoader.sortTestMethodsUsing = None
 
 class CastingApiTestCase(unittest.TestCase):
     '''Test case for the Casting Agency API.'''
 
     def setUp(self):
-        '''Set variables and init app.'''
+        '''Executes before each test. Set variables and init app.'''
         self.app = create_app()
         self.client = self.app.test_client
         self.database_url = os.environ['DATABASE_URL']
@@ -21,12 +22,38 @@ class CastingApiTestCase(unittest.TestCase):
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
             self.db.create_all()
+        
+        new_actor = Actor(
+            name='Test_Name',
+            age=31,
+            gender='f'
+        )
+        new_actor.insert()
+        new_actor = Actor.query.filter(Actor.name.ilike('%Test_Name%')).one_or_none()
+        global actor_id
+        actor_id = new_actor.id
+        
+        new_movie = Movie(
+            title='Test_Title',
+            release_date='2012-02-14'
+        )
+        new_movie.insert()
+        new_movie = Movie.query.filter(Movie.title.ilike('%Test_Title%')).one_or_none()
+        global movie_id
+        movie_id = new_movie.id
 
     def tearDown(self):
         '''Executes after each test'''
-        pass
+        actor_remove = Actor.query.get(actor_id)
+        if actor_remove:
+            actor_remove.delete()
+        
+        movie_remove = Movie.query.get(movie_id)
+        if movie_remove:
+            movie_remove.delete()
 
     def test_get_actors_success(self):
+        '''Test retrieving all actors'''
         response = self.client().get('/actors')
         data = json.loads(response.data)
 
@@ -35,6 +62,7 @@ class CastingApiTestCase(unittest.TestCase):
         self.assertTrue(data['actors'])
 
     def test_get_actors_not_found(self):
+        '''Test failing getting out of range page for actors'''
         response = self.client().get('/actors?page=100000')
         data = json.loads(response.data)
 
@@ -42,6 +70,7 @@ class CastingApiTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
 
     def test_get_movies_success(self):
+        '''Test retrieving all movies'''
         response = self.client().get('/movies')
         data = json.loads(response.data)
 
@@ -50,14 +79,74 @@ class CastingApiTestCase(unittest.TestCase):
         self.assertTrue(data['movies'])
 
     def test_get_movies_not_found(self):
+        '''Test failing getting out of range page for movies'''
         response = self.client().get('/movies?page=100000')
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 404)
         self.assertEqual(data['success'], False)
 
+    def test_add_actor_success(self):
+        '''Test adding new actor'''
+        payload = {
+            'name': 'Bozwil',
+            'age': 43,
+            'gender': 'f'
+        }
+
+        response = self.client().post('/actors', json=payload)
+        data = json.loads(response.data)
+
+        actor = Actor.query.filter(Actor.name.ilike('%Bozwil%')).one_or_none()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(payload['name'], actor.name)
+
+    def test_add_movie_success(self):
+        payload = {
+            'title': 'Wilky',
+            'release_date': '2012-02-13'
+        }
+
+        response = self.client().post('/movies', json=payload)
+        data = json.loads(response.data)
+
+        movie = Movie.query.filter(Movie.title.ilike('%Wilky%')).one_or_none()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(payload['title'], movie.title)
+
+    def test_edit_actor_success(self):
+        payload = {
+            'name': 'Test_Dubz'
+        }
+        
+        response = self.client().patch(f'/actors/{actor_id}', json=payload)
+        data = json.loads(response.data)
+
+        actor = Actor.query.get(actor_id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(payload['name'], actor.name)
+
+    def test_edit_movie_success(self):
+        payload = {
+            'title': 'Test_Moovz'
+        }
+
+        response = self.client().patch(f'/movies/{movie_id}', json=payload)
+        data = json.loads(response.data)
+
+        movie = Movie.query.get(movie_id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(payload['title'], movie.title)
+    
     def test_delete_actor_success(self):
-        actor_id = 6
         response = self.client().delete(f'/actors/{actor_id}')
         data = json.loads(response.data)
 
@@ -74,7 +163,6 @@ class CastingApiTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
 
     def test_delete_movie_success(self):
-        movie_id = 6
         response = self.client().delete(f'/movies/{movie_id}')
         data = json.loads(response.data)
 
@@ -89,68 +177,6 @@ class CastingApiTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 404)
         self.assertEqual(data['success'], False)
-
-    def test_add_actor_success(self):
-        payload = {
-            'name': 'Test',
-            'age': 43,
-            'gender': 'f'
-        }
-
-        response = self.client().post('/actors', json=payload)
-        data = json.loads(response.data)
-
-        actor = Actor.query.filter(Actor.name.ilike('%Test%')).one_or_none()
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['success'], True)
-        self.assertEqual(payload['name'], actor.name)
-
-    def test_add_movie_success(self):
-        payload = {
-            'title': 'Test',
-            'release_date': '2012-02-13'
-        }
-
-        response = self.client().post('/movies', json=payload)
-        data = json.loads(response.data)
-
-        movie = Movie.query.filter(Movie.title.ilike('%Test%')).one_or_none()
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['success'], True)
-        self.assertEqual(payload['title'], movie.title)
-
-    def test_edit_actor_success(self):
-        payload = {
-            'name': 'Test_Dubz'
-        }
-
-        actor_id = 10
-        response = self.client().patch(f'/actors/{actor_id}', json=payload)
-        data = json.loads(response.data)
-
-        actor = Actor.query.get(actor_id)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['success'], True)
-        self.assertEqual(payload['name'], actor.name)
-
-
-    def test_edit_movie_success(self):
-        payload = {
-            'title': 'Test_Moovz'
-        }
-
-        movie_id = 10
-        response = self.client().patch(f'/movies/{movie_id}', json=payload)
-        data = json.loads(response.data)
-
-        movie = Movie.query.get(movie_id)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['success'], True)
-        self.assertEqual(payload['title'], movie.title)
 
     #TODO Add fail tests for all endpoints
 
