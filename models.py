c5485b1b8982cf170cfba81eab457a7eaa83cbf2
dubiZA <iamdzh@outlook.com>
2020-07-31 15:35:45 -0600

    Yapf models.py for pep8

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
