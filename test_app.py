19b6907fca6a8caacc47394d67b7428e9284f396
dubiZA <iamdzh@outlook.com>
2020-07-31 15:38:07 -0600

    Yapf test_app.py for pep8

--- test_app.py
+++ test_app.py
@@ -10,9 +10,9 @@ TOKEN_ASSISTANT = os.environ['TOKEN_ASSISTANT']
 TOKEN_DIRECTOR = os.environ['TOKEN_DIRECTOR']
 TOKEN_PRODUCER = os.environ['TOKEN_PRODUCER']
 
+
 class CastingApiTestCase(unittest.TestCase):
     '''Test case for the Casting Agency API.'''
-
     def setUp(self):
         '''Executes before each test. Set variables and init app.'''
         self.app = create_app()
@@ -24,23 +24,18 @@ class CastingApiTestCase(unittest.TestCase):
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
             self.db.create_all()
-        
-        new_actor = Actor(
-            name='Test_Name',
-            age=31,
-            gender='f'
-        )
+
+        new_actor = Actor(name='Test_Name', age=31, gender='f')
         new_actor.insert()
-        new_actor = Actor.query.filter(Actor.name.ilike('%Test_Name%')).one_or_none()
+        new_actor = Actor.query.filter(
+            Actor.name.ilike('%Test_Name%')).one_or_none()
         global actor_id
         actor_id = new_actor.id
-        
-        new_movie = Movie(
-            title='Test_Title',
-            release_date='2012-02-14'
-        )
+
+        new_movie = Movie(title='Test_Title', release_date='2012-02-14')
         new_movie.insert()
-        new_movie = Movie.query.filter(Movie.title.ilike('%Test_Title%')).one_or_none()
+        new_movie = Movie.query.filter(
+            Movie.title.ilike('%Test_Title%')).one_or_none()
         global movie_id
         movie_id = new_movie.id
 
@@ -50,7 +45,7 @@ class CastingApiTestCase(unittest.TestCase):
         if remove_actors:
             for actor in remove_actors:
                 actor.delete()
-        
+
         remove_movies = Movie.query.all()
         if remove_movies:
             for movie in remove_movies:
@@ -59,13 +54,8 @@ class CastingApiTestCase(unittest.TestCase):
     # Success behaviour tests
     def test_get_actors_success(self):
         '''Test retrieving all actors'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'
-            }
-        response = self.client().get(
-                                    '/actors',
-                                    headers=headers
-                                    )
+        headers = {'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'}
+        response = self.client().get('/actors', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
@@ -74,13 +64,8 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_get_movies_success(self):
         '''Test retrieving all movies'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'
-        }
-        response = self.client().get(
-                                    '/movies',
-                                    headers=headers
-                                    )
+        headers = {'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'}
+        response = self.client().get('/movies', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
@@ -89,20 +74,10 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_post_actor_success(self):
         '''Test successfully adding new actor'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        payload = {
-            'name': 'Bozwil',
-            'age': 43,
-            'gender': 'f'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        payload = {'name': 'Bozwil', 'age': 43, 'gender': 'f'}
 
-        response = self.client().post(
-                                    '/actors',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().post('/actors', headers=headers, json=payload)
         data = json.loads(response.data)
 
         actor = Actor.query.filter(Actor.name.ilike('%Bozwil%')).one_or_none()
@@ -113,19 +88,10 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_post_movie_success(self):
         '''Test successfully adding a new movie'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'
-        }
-        payload = {
-            'title': 'Wilky',
-            'release_date': '2012-02-13'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'}
+        payload = {'title': 'Wilky', 'release_date': '2012-02-13'}
 
-        response = self.client().post(
-                                    '/movies',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().post('/movies', headers=headers, json=payload)
         data = json.loads(response.data)
 
         movie = Movie.query.filter(Movie.title.ilike('%Wilky%')).one_or_none()
@@ -136,18 +102,12 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_patch_actor_success(self):
         '''Test successfully modifying record for an actor'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        payload = {
-            'name': 'Test_Dubz'
-        }
-        
-        response = self.client().patch(
-                                    f'/actors/{actor_id}',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        payload = {'name': 'Test_Dubz'}
+
+        response = self.client().patch(f'/actors/{actor_id}',
+                                       headers=headers,
+                                       json=payload)
         data = json.loads(response.data)
 
         actor = Actor.query.get(actor_id)
@@ -158,18 +118,12 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_patch_movie_success(self):
         '''Test successfully modifying a movie record'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        payload = {
-            'title': 'Test_Moovz'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        payload = {'title': 'Test_Moovz'}
 
-        response = self.client().patch(
-                                    f'/movies/{movie_id}',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().patch(f'/movies/{movie_id}',
+                                       headers=headers,
+                                       json=payload)
         data = json.loads(response.data)
 
         movie = Movie.query.get(movie_id)
@@ -177,16 +131,11 @@ class CastingApiTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(payload['title'], movie.title)
-    
+
     def test_delete_actor_success(self):
         '''Test Successfully removing an actor record'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        response = self.client().delete(
-                                        f'/actors/{actor_id}',
-                                        headers=headers
-                                        )
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        response = self.client().delete(f'/actors/{actor_id}', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
@@ -195,13 +144,8 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_delete_movie_success(self):
         '''Test removing a movie records'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'
-        }
-        response = self.client().delete(
-                                        f'/movies/{movie_id}',
-                                        headers=headers
-                                        )
+        headers = {'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'}
+        response = self.client().delete(f'/movies/{movie_id}', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
@@ -211,13 +155,8 @@ class CastingApiTestCase(unittest.TestCase):
     # Tests for error behaviors
     def test_get_actors_not_found(self):
         '''Test failing getting out of range page for actors'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'
-            }
-        response = self.client().get(
-                                    '/actors?page=100000',
-                                    headers=headers
-                                    )
+        headers = {'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'}
+        response = self.client().get('/actors?page=100000', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 404)
@@ -225,13 +164,8 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_get_movies_not_found(self):
         '''Test failing getting out of range page for movies'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'
-        }
-        response = self.client().get(
-                                    '/movies?page=100000',
-                                    headers=headers
-                                    )
+        headers = {'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'}
+        response = self.client().get('/movies?page=100000', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 404)
@@ -240,13 +174,8 @@ class CastingApiTestCase(unittest.TestCase):
     def test_delete_actor_not_found(self):
         '''Test removing an actor that doesn't exists'''
         actor_id = 2000
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        response = self.client().delete(
-                                        f'/actors/{actor_id}',
-                                        headers=headers
-                                        )
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        response = self.client().delete(f'/actors/{actor_id}', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 404)
@@ -255,13 +184,8 @@ class CastingApiTestCase(unittest.TestCase):
     def test_delete_movie_not_found(self):
         '''Test removing a movie that doesn't exist'''
         movie_id = 20000
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'
-        }
-        response = self.client().delete(
-                                        f'/movies/{movie_id}',
-                                        headers=headers
-                                        )
+        headers = {'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'}
+        response = self.client().delete(f'/movies/{movie_id}', headers=headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 404)
@@ -269,20 +193,10 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_post_actor_bad_data(self):
         '''Test successfully adding new actor'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        payload = {
-            'first_name': 'Oops',
-            'age': 43,
-            'gender': 'f'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        payload = {'first_name': 'Oops', 'age': 43, 'gender': 'f'}
 
-        response = self.client().post(
-                                    '/actors',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().post('/actors', headers=headers, json=payload)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 422)
@@ -290,19 +204,10 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_post_movie_bad_data(self):
         '''Test successfully adding a new movie'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'
-        }
-        payload = {
-            'title': 'Wilky',
-            'date': '2012-02-13'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_PRODUCER)}'}
+        payload = {'title': 'Wilky', 'date': '2012-02-13'}
 
-        response = self.client().post(
-                                    '/movies',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().post('/movies', headers=headers, json=payload)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 422)
@@ -310,18 +215,12 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_patch_actor_bad_payload(self):
         '''Test successfully modifying record for an actor'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        payload = {
-            'first_name': 'Test_Dubz'
-        }
-        
-        response = self.client().patch(
-                                    f'/actors/{actor_id}',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        payload = {'first_name': 'Test_Dubz'}
+
+        response = self.client().patch(f'/actors/{actor_id}',
+                                       headers=headers,
+                                       json=payload)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 422)
@@ -329,18 +228,12 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_patch_movie_bad_payload(self):
         '''Test successfully modifying a movie record'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        payload = {
-            'name': 'Test_Moovz'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        payload = {'name': 'Test_Moovz'}
 
-        response = self.client().patch(
-                                    f'/movies/{movie_id}',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().patch(f'/movies/{movie_id}',
+                                       headers=headers,
+                                       json=payload)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 422)
@@ -359,20 +252,10 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_post_actor_no_permissions(self):
         '''Test incorrect permissions adding new actor'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'
-        }
-        payload = {
-            'name': 'Bozwil',
-            'age': 43,
-            'gender': 'f'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_ASSISTANT)}'}
+        payload = {'name': 'Bozwil', 'age': 43, 'gender': 'f'}
 
-        response = self.client().post(
-                                    '/actors',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().post('/actors', headers=headers, json=payload)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 403)
@@ -380,19 +263,10 @@ class CastingApiTestCase(unittest.TestCase):
 
     def test_post_movie_no_permissions(self):
         '''Test incorrect permissions adding a new movie'''
-        headers = {
-            'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'
-        }
-        payload = {
-            'title': 'Wilky',
-            'release_date': '2012-02-13'
-        }
+        headers = {'Authorization': f'Bearer {str(TOKEN_DIRECTOR)}'}
+        payload = {'title': 'Wilky', 'release_date': '2012-02-13'}
 
-        response = self.client().post(
-                                    '/movies',
-                                    headers=headers,
-                                    json=payload
-                                    )
+        response = self.client().post('/movies', headers=headers, json=payload)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 403)
